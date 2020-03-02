f0493459e7f41557c4669d25532b6e2192d0ffc3
hxdarrowbbom <hxdarrow@163.com>
2020-03-02 20:07:12 +0800

    xxx

--- .bash_runtime/g17_bashrc.sh
+++ .bash_runtime/g17_bashrc.sh
@@ -5,7 +5,7 @@
 # 前往工作目录
 function cdw()
 {
-	# 1 服务器logic缩写
+	# 1 服务器logic缩写, 默认前往g17
 	cd "$HOME/g17/${short_dir_map[${1-'tl'}]}"
 }
 
@@ -21,22 +21,10 @@ function ncw()
 # 结束掉所有的g17进程
 alias gstop="sh $HOME/g17/trunk/shell/killg17main.sh"
 
-# svn
-alias svnadd="svn st | grep ? | awk '{print \$2}' | xargs svn add"
-alias svnrevert="svn st | grep M | awk '{print \$2}' | xargs svn revert"
-alias svnrmnoversion="svn st | grep ? | awk '{print \$2}' | xargs rm"
-
-function svnci()
-{
-	# 1 comment
-	echo "今天你@QA了吗？[y/n]"
-	read res
-	if [[ ${res} == "y" ]]; then
-		# svn st | grep "^[AM]" | awk '{print \$2}' | xargs svn ci -m ${1}
-	else
-		echo "nono，这不行喔"
-	fi
-}
+# svn 记住格式自己来搞把
+# alias svnadd="svn st | grep ? | awk '{print \$2}' | xargs svn add"
+# alias svnrevert="svn st | grep M | awk '{print \$2}' | xargs svn revert"
+# alias svnrmnoversion="svn st | grep ? | awk '{print \$2}' | xargs rm"
 
 function svnlog()
 {
@@ -47,30 +35,31 @@ function svnlog()
 }
 
 # svnlog 查找附带后3前1行
-function svnglog()
+function svngreplog()
 {
 	# 1 要查询的子文件或文件夹，必须从Info的URL开始算
-	# 2 要查询的目标
-	# 3 limit
+	# 2 limit
+	# 3 要查询的目标
 	url=$(svn info | grep URL | head -n 1 | awk "{print \$2}")
-	svn log ${url}/${1} -l ${3-"10"} | grep ${2} -B 3 -A 1
+	svn log ${url}/${1} -l ${2-"10"} | grep ${3} -B 3 -A 1
 }
 
 # 删除pyc
 alias rmpyc="find . -name \"*.pyc\" | xargs rm"
 
 # QXZL
-g17qxzl="$HOME/g17/qxzl_trunk/logic/cmd/qcwiz/shell/g17qxzl.py"
-qxzl_center="$HOME/g17/qxzl_center"
-qxzl_trunk="$HOME/g17/qxzl_trunk"
-qxzl_remote="$HOME/g17/qxzl_remote"
-function g17qxzl()
-{
-	${g17qxzl} qxzl ${qxzl_center} ${qxzl_trunk} ${qxzl_remote}
-}
+# g17qxzl="$HOME/g17/qxzl_trunk/logic/cmd/qcwiz/shell/g17qxzl.py"
+# qxzl_center="$HOME/g17/qxzl_center"
+# qxzl_trunk="$HOME/g17/qxzl_trunk"
+# qxzl_remote="$HOME/g17/qxzl_remote"
+# function g17qxzl()
+# {
+# 	${g17qxzl} qxzl ${qxzl_center} ${qxzl_trunk} ${qxzl_remote}
+# }
 
 # 不使用了
 # alias ctl="find \"$logic_dir\" -path \"$logic_dir/autocode*\" -o -path \"$logic_dir/autocode_t*\" -o -path \"$logic_dir/common/autocode*\" -o -path \"$logic_dir/common/autocode_t*\" -o -path \"$logic_dir/char/npc*\" -o -path \"$logic_dir/common/npc*\" -o -path \"$logic_dir/maps*\" -o -path \"$logic_dir/common/map_config*\"  -o -path \"$logic_dir/fight/skills*\" -o -path \"$logic_dir/test*\" -a -prune -o -name \"*.lua\" -print | xargs ctags"
 # alias ctl="find . -name \"*.lua\" -print | xargs ctags"
 alias cte="find . -name \".cpp\" -print | xargs ctags"
-
+alias fla="find . -name \"*.lua\"| xargs grep --color -n"
+alias fc="find . -name \"*.c\" -or -name \"*.h\" -or -name \"*.cpp\" -or -name \"*.hpp\" |xargs grep --color -n"
--- .bash_runtime/g17_config.sh
+++ .bash_runtime/g17_config.sh
@@ -2,12 +2,10 @@ declare -A short_dir_map
 short_dir_map=(
 	["tl"]="trunk/logic" ["te"]="trunk/engine"
 	["bl"]="beta/logic" ["be"]="beta/engine"
-	["ex"]="ext"
+	["ext"]="ext"
 	["cl"]="center/logic" ["ce"]="center/engine"
 	["rl"]="remote/logic" ["re"]="remote/engine"
 	["qtl"]="qxzl_trunk/logic" ["qte"]="qxzl_trunk/engine"
-	["qcl"]="qxzl_center/logic" ["qce"]="qxzl_center/engine"
-	["qrl"]="qxzl_remote/logic" ["qre"]="qxzl_remote/engine"
 	["bsl"]="bson_deep/logic" ["bse"]="bson_deep/engine"
 	["ll"]="learn/logic" ["le"]="learn/engine"
 )
@@ -16,7 +14,7 @@ short_dir_map=(
 declare -A short_port_map
 short_port_map=(
 	["tl"]="6600" ["bl"]="6610" ["cl"]="6660" ["rl"]="6680"
-	["qtl"]="6630" ["qrl"]="6620" ["qcl"]="6640"
+	["qtl"]="6630" 
 	["bsl"]="6650"
 	["ll"]="6670"
 )
@@ -24,7 +22,7 @@ short_port_map=(
 declare -A short_host_map
 short_host_map=(
 	["tl"]="660" ["bl"]="661" ["cl"]="666" ["rl"]="668"
-	["qtl"]="663" ["qrl"]="662" ["qcl"]="664"
+	["qtl"]="663" 
 	["bsl"]="665"
 	["ll"]="667"
 )
